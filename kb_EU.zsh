f771e41e163e1a225e90543e5d55a862bab34049
David Asabina <vid@bina.me>
2019-08-22 09:03:21 +0200

    Configure ralt as compose

--- kb_EU.zsh
+++ kb_EU.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 function kb_EU {
-  setxkbmap -option -model dell -layout us -variant intl -option lv3:caps_switch
+  setxkbmap -option -model dell -layout us -variant intl -option "lv3:caps_switch,compose:ralt"
 }
 
 kb_EU
